f0a2051c2c9f46308ccba10fc0d1df040c91bcab
Andrew Docherty <docherty+osc@gmail.com>
2010-08-18 13:04:14 -0400

    Added compiled Cython files and removed Cython requirement

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 import os
 
 #Use setuptools for egg installs, if possible
-import setuptools
+try:
+    import setuptools
+except:
+    pass
 
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
-from Cython.Distutils.extension import Extension as CythonExtension
 
 pkg_name = "oscillate"
 pkg_version = "0,1"
@@ -19,15 +20,15 @@ except AttributeError:
     numpy_include = numpy.get_numpy_include()
 
 inc_dirs = [numpy_include]
+lib_dirs = []
+libraries = []
 
-ext_modules = [Extension(pkg_name+".odesolve", [os.path.join(pkg_name,"odesolve.pyx")], \
-                include_dirs=inc_dirs)]
+ext_modules = [Extension(pkg_name+".odesolve", [os.path.join(pkg_name,"odesolve.c")], \
+                include_dirs=inc_dirs, library_dirs=lib_dirs, libraries=libraries)]
 ext_modules += [Extension(pkg_name+".specializedconvolve", \
-                [os.path.join(pkg_name,"specializedconvolve.pyx")], include_dirs=inc_dirs)]
+                [os.path.join(pkg_name,"specializedconvolve.c")], include_dirs=inc_dirs)]
 ext_modules += [Extension(pkg_name+".noisegeneration", \
-                [os.path.join(pkg_name,"noisegeneration.pyx")], include_dirs=inc_dirs)]
-ext_modules += [Extension(pkg_name+".genericcomponent", \
-                [os.path.join(pkg_name,"genericcomponent.pyx")], include_dirs=inc_dirs)]
+                [os.path.join(pkg_name,"noisegeneration.c")], include_dirs=inc_dirs)]
 
 #Generated extensions
 
@@ -37,7 +38,6 @@ setup(
     version=pkg_version,
     author='Andrew Docherty',
     author_email='docherty@umbc.edu',
-    cmdclass = {'build_ext': build_ext},
     packages = ['oscillate'],
     zip_safe = True,
     ext_modules = ext_modules
